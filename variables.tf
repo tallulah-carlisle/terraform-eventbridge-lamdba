f89d80cdacf73e39063eb627fab459bc30a0db7b
tallulah-carlisle <99172760+tallulah-carlisle@users.noreply.github.com>
2022-06-21 20:00:18 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "lambda_function_arn" {
-  type    = list(string)
-  default = ["arn:aws:lambda:eu-west-2:889605739882:function:hello_lambda"]
-}
+# variable "lambda_function_arn" {
+#   type    = list(string)
+#   default = ["arn:aws:lambda:eu-west-2:889605739882:function:hello_lambda"]
+# }
